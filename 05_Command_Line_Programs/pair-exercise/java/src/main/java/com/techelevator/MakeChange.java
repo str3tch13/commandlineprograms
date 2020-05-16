4a82d789661a10c223e8fd01639b5281ff7b208f
Randy Proctor <randyproctor@gmail.com>
2020-05-16 11:19:48 -0400

    Added 'Exact Change' output.  Program complete and functional

--- 05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
+++ 05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
@@ -15,18 +15,35 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 		// prompt user for bill
-		System.out.println("Please enter the amount of the bill:");
+		System.out.println("Please enter the amount of the bill:" + "\n $");  //how do we make this dollar sign on the same line as the user input?
 			Scanner input = new Scanner(System.in);
 				String billAmount = input.nextLine();
 		
 		// prompt user for amount tendered
-		System.out.println("Enter the amount tendered:");
+		System.out.println("Enter the amount tendered:" + "\n $");
 				String amountTendered = input.nextLine();
 				
 		
 		// code to figure change
 				double change = Double.parseDouble(amountTendered) - Double.parseDouble(billAmount);
-		System.out.format("%.2f", change);
+				
+				if (change < 0) {
+					
+					System.out.println("Not enough payment!");
+				
+				} 
+				
+				if (change > 0) {
+				
+				
+					System.out.format("The change required is: $" + "%.2f", change);
+		
+				}
+				
+				if (change == 0) {
+					
+					System.out.println("Exact change entered.\nThank you for your payment!");
+				}
 		
 				
 				
